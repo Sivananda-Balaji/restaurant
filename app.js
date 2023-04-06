a29051929881cf3712c520ae54eac25b3ba95304
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-06 13:15:21 +0530

    Add restaurant router middleware to the app

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
-
+const restaurantRouter = require("./routes/restaurant.route");
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/restaurant", restaurantRouter);
 
 module.exports = app;
