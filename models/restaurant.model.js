192d8b856a5e0b361203524066c3bbae2f890747
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-06 13:06:29 +0530

    Add models folder to project. This folder contains the data models used in the project.

---
+++ models/restaurant.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const restaurantSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+module.exports = Restaurant;
