80af3a30188f65177c63e026af70a2a70eb57c2b
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-07 12:42:00 +0530

    Add routes for categories, restaurants,and ratings. This routes includes fetching all categories, finding restaurants by categoryName, getting restaurant details by ID and finding restaurants by rating

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -2,11 +2,19 @@ const express = require("express");
 const {
   addRestaurant,
   getRestaurants,
+  getCategories,
+  findByCategory,
+  getRestaurant,
+  findByRating,
 } = require("../controllers/restaurant.controller");
 
 const router = express.Router();
 
 router.route("/").get(getRestaurants);
 router.route("/add").post(addRestaurant);
+router.route("/categories").get(getCategories);
+router.route("/categories/:categoryName").get(findByCategory);
+router.route("/:id").get(getRestaurant);
+router.route("/rating/:ratingValue").get(findByRating);
 
 module.exports = router;
