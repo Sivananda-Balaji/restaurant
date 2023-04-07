f99016daefd8a57fe58b74301b723a9ab44c440b
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-07 22:44:31 +0530

    Add update and delete routes to the project.

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -6,15 +6,22 @@ const {
   findByCategory,
   getRestaurant,
   findByRating,
+  updateRestaurant,
+  deleteRestaurant,
+  deleteAll,
 } = require("../controllers/restaurant.controller");
 
 const router = express.Router();
 
-router.route("/").get(getRestaurants);
+router.route("/").get(getRestaurants).delete(deleteAll);
 router.route("/add").post(addRestaurant);
 router.route("/categories").get(getCategories);
 router.route("/categories/:categoryName").get(findByCategory);
-router.route("/:id").get(getRestaurant);
+router
+  .route("/:id")
+  .get(getRestaurant)
+  .put(updateRestaurant)
+  .delete(deleteRestaurant);
 router.route("/rating/:ratingValue").get(findByRating);
 
 module.exports = router;
