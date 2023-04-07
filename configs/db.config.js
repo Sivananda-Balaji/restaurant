6047a063c4297972c9114260b01e517e9bd05304
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-07 23:40:08 +0530

    Add DB-String to the project.

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   //Add DB-String to Connect
-  DB_URL: "",
+  DB_URL:
+    "mongodb+srv://Guest:0NFDy03BjieaYqI5@cluster-guest.niyjoyz.mongodb.net/restaurant_db?retryWrites=true&w=majority",
 };
